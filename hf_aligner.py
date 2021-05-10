722d2845ac29fe75bbae8db41ea62dbe2138766e
Claire D. McWhite <cmcwhite@tigergpu.princeton.edu>
2021-05-10 13:54:23 -0400

    Aligner working example

--- hf_aligner.py
+++ hf_aligner.py
@@ -13,7 +13,7 @@ from transformers import AutoTokenizer, AutoModel
  
  
  
-def get_hidden_states(seqs, model, layers):
+def get_hidden_states(seqs, model, tokenizer, layers):
 
     encoded = tokenizer.batch_encode_plus(seqs, return_tensors="pt", padding=True)
     with torch.no_grad():
@@ -52,17 +52,20 @@ def get_matches(hidden_states, seqs,seq_names):
     for a in range(len(hidden_states)): 
       complete.append(a)
       # Remove embeddings for [PAD] past the length of the original sentence
-      hidden_states_a = hidden_states[a][0:seq_lens[a] + 1]
+      hidden_states_a = hidden_states[a][1:seq_lens[a] + 1]
 
       for b in range(len(hidden_states)):
           if b in complete:
               continue
-          hidden_states_b = hidden_states[b][0:seq_lens[b]]
-          # Compare first token for sequence similarity
-          overall_sim =  util.pytorch_cos_sim(hidden_states[a][0], hidden_states[b][0])
+          hidden_states_b = hidden_states[b][1:seq_lens[b] + 1]
+          # Compare first token for sequence similarity, not the place for this
+          #overall_sim =  util.pytorch_cos_sim(hidden_states[a][0], hidden_states[b][0])
          
           # Don't compare first token [CLS] and last token [SEP]
-          cosine_scores_a_b =  util.pytorch_cos_sim(hidden_states[a][1:-1], hidden_states[b][1:-1])
+          print(len(hidden_states_a))
+          print(len(hidden_states_b))
+          cosine_scores_a_b =  util.pytorch_cos_sim(hidden_states_a, hidden_states_b)
+          print(cosine_scores_a_b)
 
           # 1 because already compared first token [CLS] to [CLS] for overall similarity
           # - 1 becuase we don't need to compare the final token [SEP]
@@ -71,22 +74,29 @@ def get_matches(hidden_states, seqs,seq_names):
              bestmatch = np.argmax(cosine_scores_a_b[i])
              #top3idx = np.argpartition(cosine_scores_a_b[i], -3)[-3:]
              #top3scores= cosine_scores_a_b[i][top3idx]
-
+             print(i)
+             print(seqs)          
+             print(seqs[a])
+             print(seq_names[a])
              node_a = '{}-{}-{}'.format(seq_names[a], i, seqs[a][i])
              node_b = '{}-{}-{}'.format(seq_names[b], bestmatch, seqs[b][bestmatch])
-             match_edges.append('{},{},{},match'.format(node_a,node_b,bestscore) 
+             match_edge = '{},{},{},match'.format(node_a,node_b,bestscore)
+             print(match_edge)
 
+             match_edges.append(match_edge)
     
 
     return match_edges
  
 def get_seq_edges(seqs, seq_names):
     seq_edges = []
+    seqs = [x.replace(" ", "") for x in seqs]
+
     for i in range(len(seqs)):
         for j in range(len(seqs[i]) - 1):
            pos1 = '{}-{}-{}'.format(seq_names[i], seqs[i][j], j)
            pos2 = '{}-{}-{}'.format(seq_names[i], seqs[i][j + 1], j + 1)
-           seq_edges.append('{},{},1,seq'.format(pos1,pos2)
+           seq_edges.append('{},{},1,seq'.format(pos1,pos2))
     return(seq_edges)
 
  
@@ -96,22 +106,26 @@ def get_similarity_network(layers, model_name, seqs, seq_names):
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     model = AutoModel.from_pretrained(model_name, output_hidden_states=True)
 
-    hidden_states = get_hidden_states(encoded, model, layers)
+    hidden_states = get_hidden_states(seqs, model, tokenizer, layers)
     print(hidden_states.shape)
 
-    match_edges = get_matches(seqs, tokenizer, model, layers)
+    match_edges = get_matches(hidden_states, seqs, seq_names)
     
     seq_edges = get_seq_edges(seqs, seq_names)
+    all_edges = seq_edges + match_edges 
+    for x in all_edges:
+      print(x)
 
     return 1
  
  
 if __name__ == '__main__':
-
+    # Embedding too focused on same amino acid. 
+    # Try out attentions
     layers = [-4, -3, -2, -1]
     model_name = 'prot_bert_bfd'
-    seqs = ['H K C Q T C G K A F N R S S T L N T H A R I H A G N P', 'Y K C K Q C G K A F A R S G G L Q K H K R T H']
-    seqs = ['H E A T', 'H A T H E A T']
+    seqs = ['A A H K C Q T C G K A F N R S S T L N T H A R I H A G N P', 'Y K C K Q C G K A F A R S G G L Q K H K R T H']
+    #seqs = ['H E A L A I', 'H E A I A L']
 
     seq_names = ['seq1','seq2']
     get_similarity_network(layers, model_name, seqs, seq_names)
