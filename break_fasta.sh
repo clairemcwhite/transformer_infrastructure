97cb59e7bbc47c04b51167318ac48ded2a385100
Claire D. McWhite <cmcwhite@della8.princeton.edu>
2022-06-02 14:44:17 -0400

    Add fasta breaker

---
+++ break_fasta.sh
@@ -0,0 +1,13 @@
+while read line
+do
+    if [[ ${line:0:1} == '>' ]]
+    then
+        tmp_outfile=${line#>}
+        tmp_outfile2=${tmp_outfile%% *}
+        outfile=${tmp_outfile2##*|}.fa
+        
+        echo $line > "$outfile"
+    else
+        echo $line >> "$outfile"
+    fi
+done < best1_human.fasta
